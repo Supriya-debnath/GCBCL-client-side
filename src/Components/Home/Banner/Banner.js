7a6559fb999400777fd9fdac916bc25b4174cef0
Supriya debnath <supriyadebnath.gcbcl@gmail.com>
2023-08-10 15:09:14 +0600

    banner add

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -6,13 +6,12 @@ import { Col, Container, Row } from 'react-bootstrap';
 const Banner = () => {
     return (
         <>
-        
-        
-                
-                <div className="banner-img">
+           
+        <div >
+            <div className="banner-img">
                 <Row>
-                <Col xs={12} md={8}>
-        <div className="banner-text banner-text2">
+                <Col xs={12} md={8} className='pt-5'>
+            <div className="banner-text banner-text2">
 
             <h1 className='animate__backInLeft animate__animated animate__zoomIn'>A Website that  <br/> <spam className="bannerh1">leads to Customers</spam></h1>
             <p className='animate__animated animate__fadeInUp'>
@@ -23,7 +22,7 @@ const Banner = () => {
         </Col>
 
        
-        <Col xs={12} md={4}>
+        <Col xs={12} md={4} className='pt-5'>
 
         <img className="img-fluid animate__backInRight" src="https://demo26.atiframe.com/wp-content/uploads/revslider/slider-940px-11/Frame1.png" alt="" />
 
@@ -31,7 +30,7 @@ const Banner = () => {
                 
         </Col>
     </Row>
-
+    </div>
     </div>
                
 
